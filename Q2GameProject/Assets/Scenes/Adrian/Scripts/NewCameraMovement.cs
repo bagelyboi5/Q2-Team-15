414dcf71118114a737821d458165cdefb1059787
MUKILTEO\1710219 <1710219@mulkilteo.wednet.edu>
2022-01-14 12:24:29 -0800

    Added my stuff

---
+++ Q2GameProject/Assets/Scenes/Adrian/Scripts/NewCameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        //transform.position = player.transform.position + offset;
+        transform.position = new Vector3(player.transform.position.x, Mathf.Clamp(player.transform.position.y, 0, 8) , -30);
+    }
+}
---
+++ Q2GameProject/Assets/Scenes/Adrian/Scripts/backgroundscolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundscolling : MonoBehaviour
+{
+
+    public float speed = 4f;
+    private Vector3 StartPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if (transform.position.y < -6.5f)
+        {
+            transform.position = StartPosition;
+        }
+    }
+}
