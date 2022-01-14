c4778c11a352ee48faafa349b2c8b317dcb160aa
MUKILTEO\1707228 <1707228@mukilteo.wednet.edu>
2022-01-14 12:49:47 -0800

    level replace

--- Q2GameProject/Assets/Scenes/Joseph/Scripts/OutOfBounds.cs
+++ Q2GameProject/Assets/Scenes/Joseph/Scripts/OutOfBounds.cs
@@ -6,16 +6,10 @@ public class OutOfBounds : MonoBehaviour
 {
     public GameObject spawnpoint;
     public GameObject Beaninventory;
-    public void Start()
-    {
-       Beaninventory = GameObject.Find("Inventory");
-    }
 
     public  void OnCollisionEnter2D(Collision2D collision)
     {
         collision.gameObject.transform.position = spawnpoint.transform.position;
-
-       Beaninventory.GetComponent<BeanScriptforinventory>().Deathtoraccon();
     }
 
 
