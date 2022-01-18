dce0fc0588bb394b69322d69a41d6ece18afd96e
MUKILTEO\1709603 <garmanmt@gmail.com>
2022-01-18 12:22:24 -0800

    save but bad cause ronald VERBENBURGER SAID SO DEAL WITH IT

---
+++ Q2GameProject/Assets/Scenes/Marcus/BeanSave.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class BeanSave : MonoBehaviour
+{
+    public int BEERN;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BEERN = GetComponent<BeanScriptforinventory>().beansspawned;
+    }
+}
