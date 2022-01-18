2e9ac15cc587f365f31b2fd1f42037303dfdfe20
MUKILTEO\1707223 <jrverburg@yahoo.com>
2022-01-18 13:18:17 -0800

    stuff added

---
+++ Q2GameProject/Assets/Scenes/Ron/Scripts/LEvelscriptcheckforboss.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LEvelscriptcheckforboss : MonoBehaviour
+{
+    public string YouNameHere;
+    public int Beancount2;
+    public GameObject effectactive;
+
+
+    public void Update()
+    {
+        Beancount2 = GetComponent<BeanScriptforinventory>().beansspawned;
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Beancount2 >= 5)
+        {
+            SceneManager.LoadScene(YouNameHere);
+            effectactive.SetActive(true);
+        }
+    }
+}
