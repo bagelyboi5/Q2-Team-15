9ad7151738797b5ff6e5c7871917f7adb708c3a0
MUKILTEO\1707223 <jrverburg@yahoo.com>
2022-01-19 12:16:03 -0800

    source tree smokin

---
+++ Q2GameProject/Assets/Scenes/Ron/Scripts/TOTHEBOSS.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TOTHEBOSS : MonoBehaviour
+{
+    public string YouNameHere;
+    public int Beancount2;
+    public GameObject Effetr;
+
+
+    public void Update()
+    {
+        Beancount2 = GetComponent<BeanScriptforinventory>().beansspawned;
+        if (Beancount2 >= 5)
+        {
+            Effetr.SetActive(true);
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (Beancount2 >= 5)
+        {
+            SceneManager.LoadScene(YouNameHere);
+        }
+    }
+}
