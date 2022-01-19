b502664e0cbba55b75e7b7488c4c3991a4e1d56d
DESKTOP-FCRCOLN\pickl <pickleagb@gmail.com>
2022-01-19 11:27:08 -0800

    At home patch

---
+++ Q2GameProject/Assets/Scenes/Adrian/Scripts/clamp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clamp : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY), 0f);
+    }
+}
